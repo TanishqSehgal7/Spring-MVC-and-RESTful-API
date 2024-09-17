a279a4a43a414d81fc5d948ee14ddaf6624398f7
TanishqSehgal7 <tsgl7246@gmail.com>
2024-09-18 00:53:03 +0530

    Persistence Layer and JPA

--- src/main/java/com/example/springbootwebtutorial/springbootwebtutorial/controllers/EmployeeController.java
+++ src/main/java/com/example/springbootwebtutorial/springbootwebtutorial/controllers/EmployeeController.java
@@ -1,13 +1,22 @@
 package com.example.springbootwebtutorial.springbootwebtutorial.controllers;
 
 import com.example.springbootwebtutorial.springbootwebtutorial.dto.EmployeeDto;
+import com.example.springbootwebtutorial.springbootwebtutorial.entities.EmployeeEntity;
+import com.example.springbootwebtutorial.springbootwebtutorial.repositories.EmployeeRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/employees")
 public class EmployeeController {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
 
     /*
          @RestController ensures that the controller is REST in nature
@@ -26,22 +35,20 @@ public class EmployeeController {
     }
 
     @GetMapping(path = "/{employeeId}")
-    public EmployeeDto getEmployeeById(@PathVariable(name = "employeeId") Long id) {
-        return new EmployeeDto(id,"Tanishq","tanishqsehgal.ts@gmail.com",
-                24, LocalDate.of(2024,1,1),true);
+    public EmployeeEntity getEmployeeById(@PathVariable(name = "employeeId") Long id) {
+        return employeeRepository.findById(id).orElse(null); // optional
     }
 
     @GetMapping()
-    public String getAllEmployees(@RequestParam(required = false, name = "inputAge") Integer age,
-                                  @RequestParam(required = false) String sortBy) {
+    public List<EmployeeEntity> getAllEmployees(@RequestParam(required = false, name = "inputAge") Integer age,
+                                                @RequestParam(required = false) String sortBy) {
                                     // optional parameter using required=false
-        return "Employee age is: " + age + ", Sorted By " + sortBy;
+        return employeeRepository.findAll();
     }
 
     @PostMapping()
-    public EmployeeDto createNewEmployee(@RequestBody EmployeeDto inputEmployee) {
-        inputEmployee.setId(100L);
-        return inputEmployee;
+    public EmployeeEntity createNewEmployee(@RequestBody EmployeeEntity inputEmployee) {
+        return employeeRepository.save(inputEmployee);
     }
 
     @PutMapping
