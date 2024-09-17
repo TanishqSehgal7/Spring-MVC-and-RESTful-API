b5cfdd7464ce6387b8952596b4d5e11326139ec4
TanishqSehgal7 <tsgl7246@gmail.com>
2024-09-17 22:02:28 +0530

    Service Layer

---
+++ src/main/java/com/example/springbootwebtutorial/springbootwebtutorial/entities/EmployeeEntity.java
@@ -0,0 +1,37 @@
+package com.example.springbootwebtutorial.springbootwebtutorial.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Employees")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor // these annotations are a part of lombok library
+public class EmployeeEntity {
+
+
+    /*
+        @Entity annotation tells the JPA/Hibernate that this class needs to
+        be converted into the table in our database
+
+        @Table gives a name to the table inside the DB explicitly
+        otherwise the class name will be set as the table name in the DB
+    */
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String email;
+    private Integer age;
+    private LocalDate dateOfJoining;
+    private Boolean isActive;
+}
