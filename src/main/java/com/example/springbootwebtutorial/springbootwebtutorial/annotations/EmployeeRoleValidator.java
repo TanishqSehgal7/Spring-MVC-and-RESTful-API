26d1b53575c62d663c170cc7eb8aaaa6ea9db197
TanishqSehgal7 <tsgl7246@gmail.com>
2024-09-21 21:32:10 +0530

    Validations and Annotations

---
+++ src/main/java/com/example/springbootwebtutorial/springbootwebtutorial/annotations/EmployeeRoleValidator.java
@@ -0,0 +1,17 @@
+package com.example.springbootwebtutorial.springbootwebtutorial.annotations;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeRoleValidator implements ConstraintValidator<EmployeeRoleValidationAnnotation, String> {
+
+
+    @Override
+    public boolean isValid(String inputRole, ConstraintValidatorContext constraintValidatorContext) {
+        List<String> roles = List.of("USER", "ADMIN");
+        return roles.contains(inputRole);
+    }
+}
--- src/main/java/com/example/springbootwebtutorial/springbootwebtutorial/dto/EmployeeDto.java
+++ src/main/java/com/example/springbootwebtutorial/springbootwebtutorial/dto/EmployeeDto.java
@@ -1,11 +1,8 @@
 package com.example.springbootwebtutorial.springbootwebtutorial.dto;
 
-import com.example.springbootwebtutorial.springbootwebtutorial.controllers.EmployeeController;
+import com.example.springbootwebtutorial.springbootwebtutorial.annotations.EmployeeRoleValidationAnnotation;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.validation.constraints.*;
 
 import java.time.LocalDate;
 
@@ -18,16 +15,38 @@ public class EmployeeDto {
     // Data Transfer Object
 
     private Long id;
+    @NotBlank(message = "Name of the employee cannot be blank!")
+    @Size(min = 3, max = 30, message = "Number of characters in the field {name} should be between 3 and 20")
     private String name;
+
+    @Email(message = "Email should be a in a valid format.")
+    @NotBlank(message = "The email of the employee cannot be blank.")
     private String email;
+
+    @Min(value = 10, message = "Age cannot be less than 10")
+    @Max(value = 70, message = "Age cannot be greater than 70")
     private Integer age;
+
+    @PastOrPresent(message = "dateOfJoining field cannot be in the future")
     private LocalDate dateOfJoining;
 
     @JsonProperty("isActive") // fix for the null value issue of isActive in response
+    @AssertTrue(message = "Employee should be active")
     private Boolean isActive;
 
-    public EmployeeDto() { // default constructor
+//    @Pattern(regexp = "^(ADMIN|USER)$", message = "Role of Employee can either be USER or ADMIN")
+    @NotBlank(message = "The role of the employee cannot be blank.")
+    @EmployeeRoleValidationAnnotation
+    private String role; // ADMIN or USER
 
+    @Positive(message = "The field salary must contain a positive value.")
+    @NotNull(message = "Salary of an employee cannot be null")
+    @Digits(integer = 6, fraction = 2, message = "The salary can be in the form xxxxxx.xx")
+    @DecimalMax(value = "100000.99")
+    @DecimalMin(value = "10000.50")
+    private Double salary;
+
+    public EmployeeDto() { // default constructor
     }
 
     public EmployeeDto(Long id, String name, String email, Integer age, LocalDate dateOfJoining, Boolean isActive) {
@@ -75,7 +94,7 @@ public class EmployeeDto {
         return dateOfJoining;
     }
 
-    public void setDateOfJoining(LocalDate dateOfJoining) {
+   public void setDateOfJoining(LocalDate dateOfJoining) {
         this.dateOfJoining = dateOfJoining;
     }
 
@@ -85,5 +104,21 @@ public class EmployeeDto {
 
     public void setIsActive(Boolean active) {
         isActive = active;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
     }
 }
